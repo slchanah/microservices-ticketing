c68939e2ad8fb6aec059eadb65ea8df23e58f545
Ivan Chan <ivan.aritaone@gmail.com>
2020-10-02 17:03:30 +0800

    chg url on frontend

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.microservices-ticketing-proj-demo.xyz',
       headers: req.headers,
     });
   } else {
